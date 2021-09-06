e00ed23effc004c11916ed40153e928f70f51d51
SJ Yoon <64758842+seojin-yoon@users.noreply.github.com>
2021-09-06 21:58:31 +0900

    Update Method.java

--- Method.java
+++ Method.java
@@ -1,1 +1,26 @@
+package JavaTest;
+
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// "public static void main(String[] args) {}" is default.  
+		int result = plus(1,3);
+		System.out.println(result);
+		
+		printTwice("OOH-AHH");
+	}
+	
+	static void printTwice(String text){
+		// No return value.
+		System.out.println(text);
+		System.out.println(text);
+	}
+	
+	static int plus(int x, int y) {
+		return x+y;
+	}
+
+}
+
 
