f4a073bf8a234cd955120e57670d0aadbed7722d
SJ Yoon <64758842+seojin-yoon@users.noreply.github.com>
2021-12-19 22:29:19 +0900

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -37,3 +37,67 @@ public class Variable {
 
 }
 
+import java.util.*;
+import java.text.*;
+public class Main {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		Account account = new Account(sc.nextInt(), sc.nextInt());
+		Account.setAnnualInterestRate(sc.nextDouble());
+		
+		account.withdraw(sc.nextDouble());
+		account.deposit(sc.nextDouble());
+		
+		
+		
+		System.out.printf("balance: %.1f\n", account.getBalance());
+		DecimalFormat format=new DecimalFormat(".###");
+		double data=account.getMonthlyInterest();
+		double data2=Math.floor(data*1000)/1000;
+		String ret=format.format(data2);
+		System.out.println("monthly interest: "+ ret);
+	}
+}
+
+class Account {
+	private static int id;
+	private static double balance;
+	private static double annualInterestRate;
+	private static Date dateCreated;
+	Account(int id1, double balance1) {
+		id=id1;
+		balance=balance1;
+	}
+	Account(){}
+	public static void setAnnualInterestRate(double x) {
+		annualInterestRate=x;
+	}
+	public static double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	public static double getMonthlyInterestRate() { 
+		return annualInterestRate/1200;
+	}
+	public static double getMonthlyInterest() {
+		return getMonthlyInterestRate() * balance;
+	}
+	public static void withdraw(double x) {
+		if(x>balance) {
+		}
+		else {
+			balance -= x;
+		}
+	}
+	public static void deposit(double x) {
+		balance += x;
+	}
+	public static int getId() {
+		return id;
+	}
+	public static double getBalance() {
+		return balance;
+	}
+}
+
+
+    
